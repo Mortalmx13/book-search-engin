1df10d2311e0e74c6521d52464e6cb5f88d80078
Charles Young <ctyoungiv@gmail.com>
2022-08-04 19:50:52 -0400

    update login so it doesnt use api anymore

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {ApolloProvider} from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  request: operation =>{
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers:{
+        authorization: token ? `Bearer ${token}`  : ''
+      }
+    });
+  },
+  uri:'/graphql'
+});
+
 function App() {
   return (
+    <ApolloClient client={client}>
+
     <Router>
       <>
         <Navbar />
@@ -13,18 +29,19 @@ function App() {
           <Route 
             path='/' 
             element={<SearchBooks />} 
-          />
+            />
           <Route 
             path='/saved' 
             element={<SavedBooks />} 
-          />
+            />
           <Route 
             path='*'
             element={<h1 className='display-2'>Wrong page!</h1>}
-          />
+            />
         </Routes>
       </>
     </Router>
+            </ApolloClient>
   );
 }
 
