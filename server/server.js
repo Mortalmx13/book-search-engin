4812d9ed19b73d4b51ace07de3bb45f7db9fcee2
Charles Young <ctyoungiv@gmail.com>
2022-08-04 18:45:24 -0400

    mutation and queries

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const server = new ApolloServer({
   context: authMiddleware
 })
 
-server.applyMiddleware({app});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,10 +28,19 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+};
+
+startApolloServer(typeDefs, resolvers);
